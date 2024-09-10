c09801577177532b2a7e296475bd129adf749e56
Bilen Liubov <bilenliubov48@mail.ru>
2024-09-10 14:45:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 21700;
+        int rublesPerBonusMile = 20;
+        int bonus = ticketPrice / rublesPerBonusMile;
+
+        System.out.println("Бонус: " + bonus);
+    }
+}
